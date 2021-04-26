a953c9241fc9ca75f479afd486655340c2bd272f
codabu <57690093+codabu@users.noreply.github.com>
2021-04-26 14:31:09 -0500

    Added files

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartialViewAndTagHelpers.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Microsoft()
+        {
+            return Redirect("https://docs.microsoft.com/en-us/aspnet/core/mvc/overview?view=aspnetcore-5.0");
+        }
+
+    }
+}
---
+++ Models/SubmitButtonTagHelper.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartialViewAndTagHelpers.Models
+{
+    [HtmlTargetElement("submit-button")]
+    public class SubmitButtonTagHelper : TagHelper
+    {
+        public override void Process (TagHelperContext context, TagHelperOutput output)
+        {
+            //make this a button element with start and end tags
+            output.TagName = "button";
+            output.TagMode = TagMode.StartTagAndEndTag;
+
+            //make the custom button a submit button
+            output.Attributes.SetAttribute("type", "submit");
+
+            //append the classes for a bootstrap button
+            string classes = "btn btn-primary";
+            output.Attributes.SetAttribute("class", classes);
+
+        }
+
+    }
+}
